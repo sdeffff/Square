b4af12642ca9919820d2bc3349f1684030a9b914
Maksym <maxpavliy@gmail.com>
2024-07-01 15:14:27 +0200

    Updates

---
+++ assets/PageNotFound-BRCNB9BI.js
@@ -0,0 +1,1 @@
+import{t,j as o}from"./index-B1vsciNn.js";const s=()=>o.jsx("h1",{className:"mt-96",children:"GTF ouf here"}),a=t(s);export{a as default};
--- assets/PageNotFound-kM_DORfU.js
+++
@@ -1,1 +0,0 @@
-import{t,j as o}from"./index-Ds7pwgIF.js";const s=()=>o.jsx("h1",{className:"mt-96",children:"GTF ouf here"}),a=t(s);export{a as default};
