e4048b2c339a21ee862cea530b05a519f1e6edc6
Maksym <maxpavliy@gmail.com>
2024-09-04 14:58:38 +0200

    Updates

---
+++ assets/PageNotFound-BvvUhgGo.js
@@ -0,0 +1,1 @@
+import{t as s,j as t}from"./index-COzUFfuH.js";const a="/Square/assets/nothing-DLi_WlKi.webp",e=()=>t.jsx("div",{className:"w-[37.5rem] h-[31rem]",children:t.jsx("img",{src:a,alt:"",className:"absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"})}),o=s(e);export{o as default};
