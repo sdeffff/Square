eafc624575c4b09f2d3ee2c624901fe19789c427
Maksym <maxpavliy@gmail.com>
2024-09-04 14:20:40 +0200

    Updates

--- assets/PageNotFound-DOXOsmg2.js
+++
@@ -1,1 +0,0 @@
-import{t as s,j as t}from"./index-CarLBLBb.js";const a="/Square/assets/nothing-CfCp4L4v.png",e=()=>t.jsx("div",{className:"w-[37.5rem] h-[31rem]",children:t.jsx("img",{src:a,alt:"",className:"absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"})}),o=s(e);export{o as default};
